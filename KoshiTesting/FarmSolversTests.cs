570df79e14242deaf6b0d8929a8f3b917a3896e8
xAsindx <asind@ASIND>
2022-10-01 20:55:20 +0700

    ModulTests v.01102022

--- KoshiTesting/FarmSolversTests.cs
+++ KoshiTesting/FarmSolversTests.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using KoshiSolvers;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Xml.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace KoshiSolvers.Tests
 {
@@ -13,22 +13,31 @@ namespace KoshiSolvers.Tests
 
         public override void SolveKoshiTask(TaskKoshi Task)
         {
-            throw new NotImplementedException();
+            return; // as if the code here has worked and returns
         }
     }
 
     [TestClass()]
     public class FarmSolversTests
     {
+        private FarmSolvers anInstance = new FarmSolvers();
+
+        public void GenerateTestName(string Name="Default")
+        {
+            BehaviorOfSolver Behavior = (BehaviorOfSolver)Convert.ToByte(1);
+            Solver testSolver = new TestSolver(Name, Behavior);
+
+            anInstance.Solvers.Add(testSolver);
+        }
+
         [TestMethod()]
         public void AddSolverTest()
         {
-            var anInstance = new FarmSolvers();
             var Name = "Testing";
             BehaviorOfSolver Behavior = (BehaviorOfSolver)Convert.ToByte(1);
-            Solver solver = new TestSolver(Name, Behavior);
+            Solver testSolver = new TestSolver(Name, Behavior);
 
-            anInstance.Solvers.Add(solver);
+            anInstance.AddSolver(testSolver);
 
             Assert.AreEqual(Name, Name, "Solver changes name");
             Assert.AreEqual(Behavior, Behavior, "Solver changes name");
@@ -36,39 +45,63 @@ namespace KoshiSolvers.Tests
 
         [TestMethod()]
         [ExpectedException(typeof(ArgumentException),
-            "SolverName shouldn't be empty, null, or white space!")]
-        public void ThrowExceptionTest()
+            "There is no such solver")]
+        public void FindSolverByName_ThrowExceptionTest()
         {
-            var anInstance = new FarmSolvers();
-            var Name = "";
-            BehaviorOfSolver Behavior = (BehaviorOfSolver)Convert.ToByte(1);
-            Solver solver = new TestSolver(Name, Behavior);
-
-            anInstance.Solvers.Add(solver);
+            var NonExistentName = "non-existent name";
+            anInstance.FindSolverByName(NonExistentName);
+            Assert.Fail();
         }
 
         [TestMethod()]
         public void FindSolverByNameTest()
         {
-            Assert.Fail();
+            var TestName = "test_name";
+            GenerateTestName(TestName);
+            int index = anInstance.FindSolverByName(TestName); //returns an index, right?
+
+            Assert.AreEqual(anInstance.Solvers[index].Name, TestName);
         }
 
         [TestMethod()]
         public void SolveProblemTest()
         {
-            Assert.Fail();
+            GenerateTestName();
+            
+            TaskKoshi Task = new TaskKoshi(1.0, 1.0, 1.0, 1.0);
+            anInstance.SolveProblem(Task);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException),
+            "There is no such solver")]
+        public void DeleteSolver_ThrowExceptionTest()
+        {
+            var NonExistentName = "non-existent name";
+            anInstance.DeleteSolver(NonExistentName);
         }
 
         [TestMethod()]
         public void DeleteSolverTest()
         {
-            Assert.Fail();
+            var ExistentName = "test_name";
+            GenerateTestName(ExistentName);
+
+            anInstance.DeleteSolver(ExistentName);
+
+
         }
 
         [TestMethod()]
+        [ExpectedException(typeof(ArgumentException),
+            "Solver with this name already exists")]
         public void CheckNameRepeatTest()
         {
-            Assert.Fail();
+            var Name = "Repiting name";
+            BehaviorOfSolver Behavior = (BehaviorOfSolver)Convert.ToByte(1);
+
+            anInstance.Solvers.Add(new TestSolver(Name, Behavior));
+            anInstance.CheckNameRepeat(Name);
         }
     }
 }
--- KoshiTesting/SolverTests.cs
+++ KoshiTesting/SolverTests.cs
@@ -6,19 +6,55 @@ using System.Text;
 
 namespace KoshiSolvers.Tests
 {
+    class SolverTestMethod : Solver
+    {
+        public SolverTestMethod(string _Name, BehaviorOfSolver _Behaviour) : base(_Name, _Behaviour) { }
+
+        public override void SolveKoshiTask(TaskKoshi Task)
+        {
+            return; // as if the code here has worked and returns
+        }
+    }
+
     [TestClass()]
     public class SolverTests
     {
         [TestMethod()]
-        public void SolveKoshiTaskTest()
+        private SolverTestMethod SolverTest()
         {
-            Assert.Fail();
+            var anInstance = new FarmSolvers();
+            var Name = "Test name";
+            BehaviorOfSolver Behavior = (BehaviorOfSolver)Convert.ToByte(1);
+            SolverTestMethod solver = new SolverTestMethod(Name, Behavior);
+
+            anInstance.Solvers.Add(solver);
+            return solver;
+
         }
 
         [TestMethod()]
-        public void SolverTest()
+        [ExpectedException(typeof(ArgumentException),
+            "SolverName shouldn't be empty, null, or white space!")]
+        public void Name_ThrowExceptionTest()
         {
-            Assert.Fail();
+            var anInstance = new FarmSolvers();
+            var Name = "";
+            BehaviorOfSolver Behavior = (BehaviorOfSolver)Convert.ToByte(1);
+            SolverTestMethod solver = new SolverTestMethod(Name, Behavior);
+
+            anInstance.Solvers.Add(solver);
         }
+
+        [TestMethod()]
+        public void SolveKoshiTaskTest()
+        {
+            var solver = SolverTest();
+
+            TaskKoshi Task = new TaskKoshi(1.0, 1.0, 1.0, 1.0);
+
+            solver.SolveKoshiTask(Task);
+        }
+
+        
     }
 }
